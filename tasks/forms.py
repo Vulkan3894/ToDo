3f10f416c5d1a2a3fa4ff78e1e80a49887e45554
Vulkan3894 <90535839+Vulkan3894@users.noreply.github.com>
2023-08-07 11:05:51 -0400

    added styling

--- tasks/forms.py
+++ tasks/forms.py
@@ -4,7 +4,7 @@ from .models import *
 from .forms import *
 
 class TaskForm(forms.ModelForm):
-    
+    title = forms.CharField(widget= forms.TextInput(attrs={'placeholder': 'Add new task...'}))
     class Meta:
         model = Task
         fields = '__all__'
